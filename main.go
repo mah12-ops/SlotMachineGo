c0f328589cc49a7ddfcc9206286d3374bab64751
mercy <mihretyirga7@gmail.com>
2026-06-01 09:08:24 -0300

    feat: add get bet function

--- main.go
+++ main.go
@@ -17,6 +17,26 @@ func getName() string {
 	return name
 }
 
+func getBet(balance uint) uint {
+	var bet uint
+
+	for true {
+		fmt.Printf("Enter your bet , or 0 to quit (balance = $%d ) : ", balance)
+		fmt.Scan(&bet)
+
+		if bet > balance {
+			fmt.Println(" Bet can not be greater that balance .")
+		} else {
+			break
+		}
+	}
+	return bet
+}
+
 func main() {
+	balance := uint(200)
+
 	getName()
+	bet := getBet(balance)
+	fmt.Println(bet)
 }
